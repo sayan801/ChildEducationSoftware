e8feaa88b67d5f9000d828e0876e1472089b0040
chandra shekhar sengupta <sayan801@gmail.com>
2012-12-22 20:48:09 +0530

    Adding Code and report

---
+++ ChildEducationSoftware/ChildEducationSoftwareEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChildEducationSoftwareEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ChildEducationSoftwareEngine")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("179cf256-c31c-4e4c-819b-e8387a7a396f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ChildEducationSoftware/ChildEducationSoftwareGUI/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChildEducationSoftwareGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            IsPlaying(false);
+        }
+
+        private void Expander_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #region IsPlaying(bool)
+        private void IsPlaying(bool bValue)
+        {
+            btnStop.IsEnabled = bValue;
+            btnMoveBackward.IsEnabled = bValue;
+            btnMoveForward.IsEnabled = bValue;
+            btnPlay.IsEnabled = bValue;
+        }
+        #endregion
+
+        #region Play and Pause
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            IsPlaying(true);
+            if (btnPlay.Content.ToString() == "Play")
+            {
+                MediaEL.Play();
+                btnPlay.Content = "Pause";
+            }
+            else
+            {
+                MediaEL.Pause();
+                btnPlay.Content = "Play";
+            }
+        }
+        #endregion
+
+        #region Stop
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            MediaEL.Stop();
+            btnPlay.Content = "Play";
+            IsPlaying(false);
+            btnPlay.IsEnabled = true;
+        }
+        #endregion
+
+        #region Back and Forward
+        private void btnMoveForward_Click(object sender, RoutedEventArgs e)
+        {
+            MediaEL.Position = MediaEL.Position + TimeSpan.FromSeconds(10);
+        }
+
+        private void btnMoveBackward_Click(object sender, RoutedEventArgs e)
+        {
+            MediaEL.Position = MediaEL.Position - TimeSpan.FromSeconds(10);
+        }
+        #endregion
+
+        #region Open Media
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Filter = "Video Files (*.wmv)|*.wmv";
+            if (ofd.ShowDialog() == true)
+            {
+                MediaEL.Source = new Uri(ofd.FileName);
+                btnPlay.IsEnabled = true;
+            }
+        }
+        #endregion
+        
+    }
+}
